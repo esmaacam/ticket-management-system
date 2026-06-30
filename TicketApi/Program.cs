6fd6b1e5e85f43fe34e7e4d4af6b75148971c82e
esmaacam <esmacam4434@gmail.com>
2026-06-30 16:50:41 +0300

    Update Program.cs

--- TicketApi/Program.cs
+++ TicketApi/Program.cs
@@ -3,10 +3,8 @@ using TicketApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
@@ -28,7 +26,6 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
